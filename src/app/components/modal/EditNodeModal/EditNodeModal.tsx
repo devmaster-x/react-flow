ce9d1e9157b1700ecb4f49294190657c3f567a1e
JW-dev0505 <shcherbakarina6@gmail.com>
2024-11-12 01:09:47 +0800

    update editnodemodal display name

--- src/app/components/modal/EditNodeModal/EditNodeModal.tsx
+++ src/app/components/modal/EditNodeModal/EditNodeModal.tsx
@@ -8,7 +8,7 @@ import "./EditNodeModal.css";
 
 const EditNodeModal: FC<EditNodeModalProps> = ({ addNode, updateNode, closeModalHandler }) => {
   const { currentNode, newNode, setNewNode } = useNodeContext();
-  const [nodeName, setNodeName] = useState(!newNode ? !currentNode ? "" : currentNode.id : newNode.id);
+  const [nodeName, setNodeName] = useState(!newNode ? !currentNode ? "" : currentNode.data.label : newNode.data.label);
 
   const actionHandler = () => {
     if (!nodeName) {
